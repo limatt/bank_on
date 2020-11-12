533042c49e3fa9b0805e1c4461dee9ee39efc3ff
Matheus Lima <matheussslima10@outlook.com>
2020-11-12 15:50:58 -0300

    teste

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Depois de 5 segundos executa a função que mostra a caixa de texto
 setTimeout(function ativaMensagem(){
     document.getElementById('caixaTexto').style.visibility = 'visible';
-},3000);
+},4000);
 
 //Quando for dado um clique na caixa de texto a mensagem some
 caixaTexto.onclick = function () {
