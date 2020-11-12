2948bb7e3ee588c942a4fc93b134016ed78c965f
Matheus Lima <matheussslima10@outlook.com>
2020-11-12 15:46:27 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Depois de 5 segundos executa a função que mostra a caixa de texto
+setTimeout(function ativaMensagem(){
+    document.getElementById('caixaTexto').style.visibility = 'visible';
+},3000);
+
+//Quando for dado um clique na caixa de texto a mensagem some
+caixaTexto.onclick = function () {
+    document.getElementById('caixaTexto').style.visibility = 'hidden';
+}
